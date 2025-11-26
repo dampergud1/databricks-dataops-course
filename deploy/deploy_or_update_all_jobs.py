bf771f1cd8b53e66ac6e61ccd416fec72a11b38f
dampergud1 <soelvstroem@hotmail.com>
2025-11-26 10:11:00 +0000

    new path

--- deploy/deploy_or_update_all_jobs.py
+++ deploy/deploy_or_update_all_jobs.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 deploy_notebooks = [
     str(path)
-    for path in Path("/Workspace/Repos/dampergud1/databricks-dataops-course/orgs/acme/domains/transport/projects/").glob(
+    for path in Path("/Users/soelvstroem@hotmail.com/databricks-dataops-course/orgs/acme/domains/transport/projects/").glob(
         "**/deploy"
     )
     if "example_" not in str(path)
